bfdf34f47c979925874c697990465501fff6b5d7
ahmadalmezaal <ahmad.hmazaal@gmail.com>
2023-12-12 14:38:41 +0000

    feat: add api key auth

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { seedersRoutes } from "./seeders.routes";
+import { adminController } from "../controllers/admin.controller";
+import { verifyApiKey } from "../middlewares/validateApiKey.middleware";
 
 export const adminRoutes = Router();
 
+adminRoutes.use(verifyApiKey);
 adminRoutes.use('/seeders', seedersRoutes);
+adminRoutes.post('/api-key/generate', adminController.generateApiKey);
